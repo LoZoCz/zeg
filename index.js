bb5563e43c595d5398057f581912c3f2e87c4182
LoZo <m.czernik12@gmail.com>
2023-09-11 22:30:16 +0200

    little changes

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const balls = document.querySelectorAll(".ball")
+const balls = document.querySelectorAll(".ball");
 
-balls.forEach(ball => {
-    ball.textContent = Math.floor(Math.random() * 20)
-})
+balls.forEach((ball) => {
+  ball.textContent = Math.floor(Math.random() * 20);
+});
